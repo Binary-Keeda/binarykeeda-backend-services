5ed2c65abe01e8d406f3281d50f40f95778e7ec5
Aryan Bhandari <aryanbhandari4077@gmail.com>
2025-06-08 11:15:27 +0530

    auto submit

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ import reviewRouter from "./services/testSeriesService/routes/CodeReview.js";
 import morgan from "morgan";
 import profileRouter from "./services/userServices/routes/profileRoutes.js";
 import nodemailer from 'nodemailer'
+import { TestResponse } from "./services/testSeriesService/models/TestResponse.js";
 configDotenv();
 
 const app = express();
@@ -82,6 +83,28 @@ app.use('/user/profile/' , profileRouter);
 // end point for api.binarykeeda.com
 app.get('/', (req, res) => {
   res.redirect('https://binarykeeda.com');
+});
+
+
+app.post('/submit/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updated = await TestResponse.findByIdAndUpdate(
+      id,
+      { isSubmitted: true },
+      { new: true }
+    );
+
+    if (!updated) {
+      return res.status(404).json({ message: 'Test response not found' });
+    }
+
+    res.json({ message: 'Test submitted successfully', data: updated });
+  } catch (error) {
+    console.error('Error updating isSubmitted:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
